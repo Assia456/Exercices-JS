4f33f9efee9039540e243c6d01a36c75abebbd3c
Assia456 <assiaidali456@gmail.com>
2025-05-19 17:26:35 +0200

    jytui

--- app.js
+++ app.js
@@ -1,5 +1,12 @@
 fetch('https://fakestoreapi.com/products')
   .then(response => response.json())
-  .then(data => console.log(data));
+  .then(data => console.log(data=>{
+    //ici et uniquement ici j'ai acces a mon tablea de données
 
-  
+  }
+
+
+
+
+));
+
